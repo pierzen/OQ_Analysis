0aa79404cb28ca6af545c5afea34b5fd86a76d2d
Pierre Beland <infosbelas-am@yahoo.fr>
2019-09-29 20:39:41 -0400

    adding tot_spike to the eval json analysis variable
    spikes warnings let select polygons with some angles less then 10 degrees.  These are generally points moved away by error.

--- sql/Analysis/OQ_Building_Analysis.sql
+++ sql/Analysis/OQ_Building_Analysis.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE FUNCTION public.OQ_Building_Analysis(id bigint, linestring geometry, 
-	tags hstore default '"building" => "multipolygon", "QA" => "oq_polygon_ortho"') 
+	tags hstore default '"building" => "multipolygon", "QA" => "oq_polygon_ortho"') 									 
 RETURNS json AS $PROC$
 DECLARE
 	-- OQ_Building_Analysis Performs Topology Validation and Form Analysis on buildings
@@ -50,11 +50,10 @@ DECLARE
 	angle_r2_p float;
 	angle_rr_m float;
 	angle_rr_p float;
-	angle_rr2_m float;
-	angle_rr2_p float;
 	angle_d_m float;
 	angle_d_p float;
 	angle_dd_m float;
+	angle_spike float;
 	angle_dd_p float;
 	deg_var_max float;
 	l integer;
@@ -66,11 +65,13 @@ DECLARE
 	lv_azimuth float[];
 	lp_angle float[];
 	lp_type_angle text[];
+	tot_type_angle jsonb;
+	tot_grp_angle jsonb;
 	grptag text;
 	sgrptag text;
 	type_polygon text;
 	npoints text;
-	tangles int;
+	tangles integer;
 	mangle float;
 	pm1 integer;
 	p0 integer;
@@ -78,23 +79,25 @@ DECLARE
 	p2 integer;
 	angle_deb integer;
 	angle_end integer;	
-	gti int;
-	tnd int;
-	tir int;
-	trr int;
-	tr int;	
-	tq int;
-	tqq int;
-	tqqq int;
-	th int;
-	thh int;
-	td int;
-	tdd int;
-	iflag int;
+	gti integer;
+	tnd integer;
+	tir integer;
+	trr integer;
+	tr integer;	
+	tq integer;
+	tqq integer;
+	tqqq integer;
+	th integer;
+	thh integer;
+	td integer;
+	tdd integer;
+	iflag integer;
+	tspike integer;
 	poly_types_angle text;
 	type_geom text;
 	tpoints text;
 	tazimuths text;
+	teval text;
 	tresultat text;
 	resultat text[];
 BEGIN
@@ -136,8 +139,9 @@ BEGIN
 	angle_d_p  = 180 + tolerance_angle_1;
 	angle_dd_m = 180 - tolerance_angle_2;
 	angle_dd_p = 180 + tolerance_angle_2;
+	angle_spike = 10.0;
 	nb_points = ST_NPoints(linestring);
-	
+
 	case WHEN exist(tags, 'building') THEN grptag:='building';
 	WHEN exist(tags, 'landuse') THEN grptag:='landuse';
 	WHEN exist(tags, 'amenity') THEN grptag:='amenity';
@@ -158,18 +162,19 @@ BEGIN
 	IF grptag='building' THEN sgrptag='FB';
 	ELSE sgrptag='FO';
 	END IF;
-
-	IF (ST_NPoints(linestring)< 4 OR ST_IsClosed(linestring) is false)
-	THEN return format('{ "grptag":"%s", "flag": "1", "npoints": "%s", "type_polygon": "Open", "nb_angles": 0,
-				"angles": "{NULL}", "type_geom": "{NULL}", "poly_types_angle": "{NULL}", "l_polygon": "{NULL}" }', grptag, ST_NPoints(linestring))::json;
+	IF linestring is null or ST_IsValid(linestring) is false
+	THEN return format('{ "grptag":"%s", "flag": "1", "teval": "Err", "npoints": "%s", "type_geom": "{NULL}", "poly_types_angle": "{NULL}", "type_polygon": "Invalid", "nb_angles": 0,	"angle_list": "{NULL}", "type_angle_list": "{NULL}" }', grptag, ST_NPoints(linestring))::json;
+	ELSIF (ST_NPoints(linestring)< 4 OR ST_IsClosed(linestring) is false)
+	THEN return format('{ "grptag":"%s", "flag": "1", "teval": "Err", "npoints": "%s", "type_geom": "{NULL}", "poly_types_angle": "{NULL}", "type_polygon": "Open", "nb_angles": 0,	"angle_list": "{NULL}", "type_angle_list": "{NULL}" }', grptag, ST_NPoints(linestring))::json;
 	ELSIF ST_NPoints(linestring)> 3 and ST_IsValid(ST_Polygon(linestring, 4326)) is false
-	THEN return format('{ "grptag":"%s", "flag":"1", "npoints": "%s", "type_polygon": "Invalid", "nb_angles": 0,
-				"angles": "{NULL}", "type_geom": "{NULL}", "poly_types_angle": "{NULL}", "l_polygon": "{NULL}" }', grptag, ST_NPoints(linestring))::json;
+	THEN return format('{ "grptag":"%s", "flag": "1", "teval": "Err", "npoints": "%s", "type_geom": "{NULL}", "poly_types_angle": "{NULL}", "type_polygon": "Invalid", "nb_angles": 0,	"angle_list": "{NULL}", "type_angle_list": "{NULL}" }', grptag, ST_NPoints(linestring))::json;
 	ELSE
 		IF ST_IsClosed(linestring) = False OR ST_IsValid(ST_Polygon(linestring, 4326)) is false
 		THEN polygon=null;
 		ELSE polygon=ST_Polygon(linestring, 4326);
 		END IF;
+		--raise info 'id % nb_points %',id, nb_points;
+
 		nb_points_m = nb_points-1;
 		FOR k IN 1..nb_points 
 		LOOP
@@ -246,10 +251,13 @@ BEGIN
 		tqqq=0;
 		td=0;
 		tdd=0;
+		tspike=0;
 		FOR k IN angle_deb..nb_points_m 
 		LOOP
 			IF k=nb_points_m THEN l=angle_end;
 			ELSE l=k+1;
+			END IF;
+			IF lp_angle [k] <= angle_spike  then tspike = tspike + 1;
 			END IF;
 			IF k=nb_points_m and ST_IsClosed(linestring) = false THEN lp_type_angle[k] = 'v';
 			-- ignore 180 degres (point in a straight line) for orthogonal measure
@@ -266,7 +274,6 @@ BEGIN
 			elsif lp_angle [k] between angle_qq_m and angle_qq_p then lp_type_angle[k] = 'qq';
 			elsif lp_angle [k] between angle_qq2_m and angle_qq2_p then lp_type_angle[k] = 'qq';
 			elsif lp_angle [k] between angle_rr_m and angle_rr_p then lp_type_angle[k] = 'rr';
-			elsif lp_angle [k] between angle_rr2_m and angle_rr2_p then lp_type_angle[k] = 'rr';
 			elsif lp_angle [k] between angle_hh_m and angle_hh_p then lp_type_angle[k] = 'hh';
 			elsif lp_angle [k] between angle_hh2_m and angle_hh2_p then lp_type_angle[k] = 'hh';
 			elsif lp_angle [k] between angle_hh3_m and angle_hh3_p then lp_type_angle[k] = 'hh';
@@ -290,28 +297,6 @@ BEGIN
 			END IF;
 		END LOOP;
 		
-		IF (ST_IsClosed(linestring)= false OR nb_points<4)
-			and (exist(tags, 'building') or exist(tags, 'landuse') OR exist(tags, 'leisure')
-			 or  exist(tags, 'natural')  or exist(tags, 'man_made'))
-		THEN type_polygon='Open';
-		ELSIF (nb_points=4)  
-			 and (exist(tags, 'building') or exist(tags, 'landuse') OR exist(tags, 'leisure')
-			 or   exist(tags, 'natural')  or exist(tags, 'man_made'))
-		THEN type_polygon=format('Small');
-		ELSIF ST_isvalid(polygon)=true and ST_Area(polygon::geography)<2.0
-		THEN type_polygon=format('Micro');
-		elsif ((th+tq+tr)>4) and (tq+tr+th+td) = nb_points_m then type_polygon='r';
-		elsif grptag<>'building' then type_polygon='nd';
-		-- flag building polygon only
-		elsif tir > 0 then type_polygon = 'ir';
-		elsif tqqq> 0 then type_polygon = 'rrr';
-		elsif (thh+tqq+trr+tdd)> 0
-		then type_polygon = 'rr';
-		elsif (th+tq+tr)>0 
-		then type_polygon = 'r';
-		else type_polygon='-nd-';
-		END IF;
-
 		poly_types_angle='';
 		if tq>0 then poly_types_angle:=trim(poly_types_angle ||'-q');
 		end if;
@@ -343,13 +328,69 @@ BEGIN
 			WHEN tir>0 THEN type_geom:='ir';
 			ELSE type_geom:='-nd-';
 		END CASE;
-		IF ltrim(type_geom) in ('','reg') then iflag=0;
-		ELSE iflag=1;			   
-		END IF;
-			
+		-- type_polygon
+		CASE 
+			WHEN (ST_IsClosed(linestring)= false
+			OR nb_points<4)
+			THEN type_polygon='Open';
+			WHEN (nb_points=4) 
+			THEN type_polygon=format('Small');
+			WHEN ST_isvalid(polygon)=true and ST_Area(polygon::geography)<2.0
+			THEN type_polygon=format('Micro');
+			WHEN tir>0 THEN type_polygon:='ir';
+			WHEN tqqq>0 THEN type_polygon='rrr';
+			WHEN (tqq+trr+thh+tdd)>0 THEN type_polygon='rr';
+			ELSE type_polygon = 'r';
+		END CASE;		
+
+		CASE 
+			WHEN type_polygon IN ('Open','Micro','Invalid','Small') then teval='Err';
+			WHEN type_polygon='r' THEN teval=''; 
+			ELSE teval='FB';
+		END CASE;		
+		CASE 
+			WHEN type_polygon IN ('Open','Micro','Invalid','Small') then iflag=1;
+			WHEN type_polygon='r' THEN iflag=0; 
+			ELSE iflag=1;
+		END CASE;		
+
 		npoints = nb_points::text;
-		tresultat=format('{ "grp_tag":"%s", "flag":"%s",  "nb_points": "%s", "type_geom": "%s", "poly_types_angle": "%s", "type_polygon": "%s", "nb_angles": %s, "angle_list": "%s", "type_angle_list": "%s"}', grptag, iflag, npoints, type_geom, poly_types_angle,type_polygon, nb_points_m-td, lp_angle, lp_type_angle);
+		--raise notice 'npoints %', npoints;
+		/*
+		--tot_type_angle=[];
+		tot_type_angle['nd'] = tnd;
+		tot_type_angle['ir'] = tir;
+		tot_type_angle['rr'] = trr;
+		tot_type_angle['r'] = tr;
+		tot_type_angle['q'] = tq;
+		tot_type_angle['qq'] = tqq;
+		tot_type_angle['qqq'] = tqqq;
+		tot_type_angle['h'] = th;
+		tot_type_angle['hh'] = thh;
+		tot_type_angle['d'] = td;
+		tot_type_angle['dd'] = tdd;
+		*/
+		tot_type_angle=format('{"nd": %s}',  tnd)::jsonb;
+		tot_type_angle=tot_type_angle|| format('{"ir": %s}',  tir)::jsonb;
+		tot_type_angle=tot_type_angle|| format('{"rr": %s}',  trr)::jsonb;
+		tot_type_angle=tot_type_angle|| format('{"r": %s}',  tr)::jsonb;
+		tot_type_angle=tot_type_angle|| format('{"q": %s}',  tq)::jsonb;
+		tot_type_angle=tot_type_angle|| format('{"qq": %s}',  tqq)::jsonb;
+		tot_type_angle=tot_type_angle|| format('{"qqq": %s}',  tqqq)::jsonb;
+		tot_type_angle=tot_type_angle|| format('{"h": %s}',  th)::jsonb;
+		tot_type_angle=tot_type_angle|| format('{"hh": %s}',  thh)::jsonb;
+		tot_type_angle=tot_type_angle|| format('{"d": %s}',  td)::jsonb;
+		tot_type_angle=tot_type_angle|| format('{"dd": %s}',  tdd)::jsonb;
+		--raise info 'tot_type_angle %', tot_type_angle;
+		
+		tot_grp_angle=format('{"r": %s}',  tr+tq+td )::jsonb;
+		tot_grp_angle=tot_grp_angle|| format('{"rr": %s}',  trr+tqq+tdd+thh)::jsonb;
+		tot_grp_angle=tot_grp_angle|| format('{"ir": %s}',  tir+tqqq)::jsonb;
+		--raise info 'tot_grp_angle %', tot_grp_angle;
+		
+		tresultat=format('{ "grp_tag":"%s", "flag":"%s",  "teval": "%s", "nb_points": "%s", "type_geom": "%s", "poly_types_angle": "%s", "type_polygon": "%s", "nb_angles": %s, "angle_list": "%s", "type_angle_list": "%s", "tot_type_angle": %s , "tot_grp_angle": %s , "tot_spike": %s }', grptag, iflag, teval, npoints, type_geom, poly_types_angle,type_polygon, nb_points_m-td, lp_angle, lp_type_angle, tot_type_angle, tot_grp_angle, tspike);
 		RETURN tresultat;
 	END IF;
 END
-$PROC$ LANGUAGE plpgsql;
+$PROC$ LANGUAGE plpgsql;																												
+					
